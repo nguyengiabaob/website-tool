ed7c8b9a2a2d0177bc45e6f1e0c973f1000ff3d7
baong <baong@vntt.com.vn>
2026-01-07 09:02:01 +0700

    fix error libnss3.so88

--- public/src/app.js
+++ public/src/app.js
@@ -133,7 +133,7 @@ document.getElementById("fetch-btn").addEventListener("click", async () => {
     if (!res.ok) {
       const j = await res.json().catch(() => ({}));
       return setError(
-        j.error || `Fetch failed 123: ${res.status} ${res.json()}`
+        j.error || `Fetch failed 123: ${res.status} ${res.text()}`
       );
     }
     const j = await res.json();
